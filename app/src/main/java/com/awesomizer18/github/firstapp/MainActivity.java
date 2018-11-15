cffe611795754535b2ca616df09b51f2a6fed289
Jackson Bowler <awesomizer18@gmail.com>
2018-11-15 18:21:04 -0500

    activiated button click listener

--- app/src/main/java/com/awesomizer18/github/firstapp/MainActivity.java
+++ app/src/main/java/com/awesomizer18/github/firstapp/MainActivity.java
@@ -2,19 +2,41 @@ package com.awesomizer18.github.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupContinueButton();
     }
 
-    public void buttonOnClick(View v) {
-        Button button=(Button) v;
-        ((Button) v).setText("clicked");
+    private void setupContinueButton() {
+        Button btn1 = (Button) findViewById(R.id.buttonContinue);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "This is a log message!");
+                Toast.makeText(getApplicationContext(), "Ok", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+    private void setupYoutubeButton() {
+        Button btn1 = (Button) findViewById(R.id.youtube);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Thank you!", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 }
